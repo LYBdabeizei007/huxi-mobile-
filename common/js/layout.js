7c748e14c11b17853d9b01657f92f368dbef8b50
LYBdabeizei007 <1293649430@qq.com>
2020-05-17 18:17:26 +0800

    甲伟达

--- common/js/layout.js
+++ common/js/layout.js
@@ -1,8 +1,3 @@
-$(function () {
-  $('#close').click(function () {
-    $(this).parent().parent().hide()
-  })
-})
 !function(n){
   var  e=n.document,
     t=e.documentElement,
@@ -10,8 +5,24 @@ $(function () {
     d=i/100,
     o="orientationchange"in n?"orientationchange":"resize",
     a=function(){
-      var n=t.clientWidth||320;n>750&&(n=750);
+      var n=t.clientWidth||375;n>750&&(n=750);
       t.style.fontSize=n/d+"px"
     };
   e.addEventListener&&(n.addEventListener(o,a,!1),e.addEventListener("DOMContentLoaded",a,!1))
 }(window);
+// window.onload = function(){
+//   getRem(750,100)
+// };
+// window.onresize = function(){
+//   getRem(750,100)
+// };
+// function getRem(pwidth,prem){
+//   var html = document.getElementsByTagName("html")[0];
+//   var oWidth = document.body.clientWidth || document.documentElement.clientWidth;
+//   html.style.fontSize = oWidth/pwidth*prem + "px";
+// }
+$(function () {
+  $('#close').click(function () {
+    $(this).parent().parent().hide()
+  })
+})
